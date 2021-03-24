18c20849f5d94a2d9bb22a6d871fb3c9f74ce959
Swapnil Mathur <68532286+Swapnil794@users.noreply.github.com>
2021-03-24 22:12:44 +0530

    Update candidate.js

--- routes/candidate.js
+++ routes/candidate.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var router = express.Router();
 const {check} = require('express-validator');
-const {insertCandidate} = require('../controllers/candidate') 
+const {insertCandidate,getCandidate} = require('../controllers/candidate') 
 
 // insert candidate
 router.post('/insertcandidate',[
     check("name","name atlest 3 char").isLength({min:3}),
     check("email","must be an email type").isEmail(),
-    check("rollNumber","roll number can not be duplicate")
+    check("rollNumber","roll must be a unique number").isNumeric()
 ],insertCandidate);
+
+// get candidate
+router.get('/getcandidate',[
+    check("email","must be an email type").isEmail()
+],getCandidate);
 
 module.exports = router;
